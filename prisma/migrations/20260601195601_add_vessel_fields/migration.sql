49a03d185a1d3c6f758a52500e61e1be324212fb
Stan Gommans <stan.gommans@indicia.nl>
2026-06-01 21:56:25 +0200

    chore(database): add migration for vessel fields in Review model

---
+++ prisma/migrations/20260601195601_add_vessel_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Review" ADD COLUMN "purchaseCurrency" TEXT;
+ALTER TABLE "Review" ADD COLUMN "purchasePrice" REAL;
+ALTER TABLE "Review" ADD COLUMN "vessel" TEXT;
+ALTER TABLE "Review" ADD COLUMN "vesselSize" TEXT;
+ALTER TABLE "Review" ADD COLUMN "vesselSizeMl" REAL;
